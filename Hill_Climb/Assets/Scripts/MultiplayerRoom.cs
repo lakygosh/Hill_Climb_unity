cd7fddb5a31e583ec9ef0334964db53411037e3f
AleksandarGazikalovic <aleksandargazikalovic@gmail.com>
2023-07-17 14:45:45 +0200

    Added Multiplayer waiting room

---
+++ Hill_Climb/Assets/Scripts/MultiplayerRoom.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MultiplayerRoom : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI player1Name;
+    [SerializeField] private TextMeshProUGUI player2Name;
+    [SerializeField] private SpriteRenderer player2Online;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player2Name.text!="Player 2")
+        {
+            player2Online.color=Color.green;
+        }
+        
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
