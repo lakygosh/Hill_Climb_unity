5aa1cc2359904308c57a8fb38b340140598532be
lakygosh <lazar22.gosic@gmail.com>
2023-09-04 18:28:37 +0200

    RupaV1
    Prva verzija rupe i jos po neka sranja

--- Hill_Climb/Assets/Scripts/ScoreController.cs
+++ Hill_Climb/Assets/Scripts/ScoreController.cs
@@ -8,10 +8,10 @@ public class ScoreController : MonoBehaviour
 {
 
     [SerializeField] private TextMeshProUGUI _distanceText;
-    private Transform _playerTrans;
-    private Player _player;
+    [SerializeField] private Transform _playerTrans;
+    [SerializeField] private Player player;
     public static ScoreController instance;
-    public Vector2 distance;
+    internal Vector2 distance;
     private void Awake()
     {
         if (instance == null) 
@@ -24,44 +24,32 @@ public class ScoreController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _player = PlayerSpawner.spawnedObject.GetComponent<Player>();
-        _playerTrans = _player.transform;
         _startPosition = _playerTrans.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!GameManager.IsGameOver)
-        {
-            distance = (Vector2)_playerTrans.position - _startPosition;
-            distance.y = 0f;
+        distance = (Vector2)_playerTrans.position - _startPosition;
+        distance.y = 0f;
 
-            if (distance.x < 0f)
-            {
-                distance.x = 0f;
-            }
+        if (distance.x < 0f)
+        { 
+            distance.x = 0f;
+        }    
 
-            _distanceText.text = distance.x.ToString("F0") + " m";
-        }
+        _distanceText.text = distance.x.ToString("F0") + " m";
     }
     
     
-    public void saveScore(ref TextMeshProUGUI score)
+    public void newBestScore()
     {
-        if(distance.x>PlayerManager.GetSelectedPlayer().playerData.BestScore)
+        if(distance.x>player.playerData.BestScore)
         {
-            PlayerManager.GetSelectedPlayer().playerData.BestScore = distance.x;
-            Debug.Log(_player.playerData.BestScore);
-            StartCoroutine(PlayerManager.SaveScore(PlayerManager.GetSelectedPlayer()));
-            score.color = Color.green;
-            score.text = "New Best Score: " + distance.x.ToString("F0") + " m";
+            player.playerData.BestScore = distance.x;
+            Debug.Log(player.playerData.BestScore);
+            StartCoroutine(PlayerManager.SaveBestScore(player));
         }
-        else
-        {
-            score.text = "Score: " + instance.distance.x.ToString("F0") + " m";
-        }
-
     }
     
     
